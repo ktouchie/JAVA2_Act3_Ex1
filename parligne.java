88de7b3827cd41a36ad73bbdd04e69b450a240aa
Karin Touchie <ktouchie@gmail.com>
2016-01-04 11:41:41 +0100

    Test array length

---
+++ parligne.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class parligne {
+    
+    public static void main(String[] args){
+        
+        int array[][] = {{1,2,3,4,5},{2,4,6,8,10},{3,6,9,12,15},{4,8,12,16,20}};
+        
+        System.out.println(array[][].length);
+        
+    }
+    
+}
