873feb9443d522bd8673810205c4be8a08fbc0b5
Karin Touchie <ktouchie@gmail.com>
2016-01-04 11:48:58 +0100

    Add for loop to print array line by line

--- parligne.java
+++ parligne.java
@@ -5,7 +5,12 @@ class parligne {
         
         int array[][] = {{1,2,3,4,5},{2,4,6,8,10},{3,6,9,12,15},{4,8,12,16,20}};
         
-        System.out.println(array[][].length);
+        for (int n = 0; n<array.length; n++) {
+            for (int m = 0; m<array[n].length; m++) {
+                System.out.print(array[n][m] + " ");
+            }
+            System.out.println("");
+        }
         
     }
     
